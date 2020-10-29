ebb1f127ba32bdd920d801f2ae7942aaa2edc0bc
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2020-10-29 15:02:20 +0200

    Add exercise

---
+++ Inheritance/Lab/04.RandomList/RandomList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random rand = new Random();
+
+            int index = rand.Next(0, Count);
+
+            string toRemove = this[index];
+
+            Remove(toRemove);
+
+            return toRemove;
+        }
+    }
+}
---
+++ Inheritance/Lab/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        }
+
+        public void AddRange(List<string> list)
+        {
+            for (int i = 0; i <= list.Count; i++)
+            {
+                Push(list[i]);
+            }
+        }
+    }
+}
