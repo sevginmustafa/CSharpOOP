992cb4fe4efed49f15fcf6781e15b2e209fbeffb
Sevgin Mustafa <sevgin_6_1996@abv.bg>
2020-10-31 23:40:07 +0200

    Add exercise

---
+++ Inheritance/Exercise/06.Animals/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public abstract string ProduceSound();
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(GetType().Name);
+            sb.AppendLine($"{Name} {Age} {Gender}");
+            sb.Append($"{ProduceSound()}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Inheritance/Exercise/06.Animals/StartUp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "Beast!")
+            {
+                string[] info = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+                string name = info[0];
+                int age = int.Parse(info[1]);
+                string gender = info[2];
+
+
+                if (age > 0 && (gender == "Male" || gender == "Female"))
+                {
+                    Animal animal = null;
+
+                    if (input == "Dog")
+                    {
+                        animal = new Dog(name, age, gender);
+                    }
+                    else if (input == "Cat")
+                    {
+                        animal = new Cat(name, age, gender);
+                    }
+                    else if (input == "Frog")
+                    {
+                        animal = new Frog(name, age, gender);
+                    }
+                    else if (input == "Kitten")
+                    {
+                        animal = new Kitten(name, age);
+                    }
+                    else if (input == "Tomcat")
+                    {
+                        animal = new Tomcat(name, age);
+                    }
+
+                    animals.Add(animal);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+    }
+}
