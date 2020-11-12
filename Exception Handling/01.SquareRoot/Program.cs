71eecf67ea5cebe6c8d5855b0d9d51c7283717f6
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2020-11-12 14:59:54 +0200

    Add exercise

---
+++ Exception Handling/01.SquareRoot/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01.SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
