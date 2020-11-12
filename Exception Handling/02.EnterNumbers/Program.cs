e4afbc09a966fa50834237ff18d475b83fde82f1
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2020-11-12 15:10:43 +0200

    Add exercise

---
+++ Exception Handling/02.EnterNumbers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _02.EnterNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            ReadNumber(start, end);
+        }
+
+        static void ReadNumber(int start, int end)
+        {
+            int currentNumber = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    int number = int.Parse(Console.ReadLine());
+
+                    if (number > 1 && number < 100
+                        && number >= start && number <= end
+                        && number > currentNumber)
+                    {
+                        currentNumber = number;
+                    }
+                    else
+                    {
+                        throw new Exception("You've entered an invalid number!"
+                            + Environment.NewLine + "Try all over again!");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    i = -1;
+                    currentNumber = 0;
+                }
+            }
+            Console.WriteLine($"You've successfully entered 10 valid numbers!"
+                + Environment.NewLine + "Congratulations, you're the smartest person in the world!");
+        }
+    }
+}
