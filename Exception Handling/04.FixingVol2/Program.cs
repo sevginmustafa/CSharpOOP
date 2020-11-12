500806c597367836955996d5ee0227b8b4cd4964
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2020-11-12 15:24:24 +0200

    Add exercise

---
+++ Exception Handling/04.FixingVol2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04.FixingVol2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 30;
+            int num2 = 60;
+
+            byte result;
+
+            try
+            {
+                result = Convert.ToByte(num1 * num2);
+                Console.WriteLine($"{num1} * {num2} = {result}");
+            }
+            catch (Exception)
+
+            {
+                Console.WriteLine("Value was either too large or too small for an unsigned byte.");
+            }
+        }
+    }
+}
