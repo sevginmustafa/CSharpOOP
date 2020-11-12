9165f6e7c117244d10091d71e608cc6b3933d8e1
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2020-11-12 15:23:21 +0200

    Add exercise

---
+++ Exception Handling/03.Fixing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _03.Fixing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weekdays = new string[]
+            {
+               "Sunday","Monday","Tuesday","Wednesday","Thursday"
+            };
+
+            try
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine(weekdays[i]);
+                }
+                Console.ReadLine();
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Index was outside the bounds of the array!");
+            }
+        }
+    }
+}
