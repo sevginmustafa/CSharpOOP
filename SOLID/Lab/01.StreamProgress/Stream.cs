03b9c4c32a932c22adf7d6ebba166d6dc6923ff8
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2020-11-16 13:37:48 +0200

    Add exercise

---
+++ SOLID/Lab/01.StreamProgress/Stream.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.Stream_Progress
+{
+    public abstract class Stream
+    {
+        public Stream(int length, int bytesSent)
+        {
+            Length = length;
+            BytesSent = bytesSent;
+        }
+
+        public int Length { get; }
+
+        public int BytesSent { get; }
+    }
+}
---
+++ SOLID/Lab/01.StreamProgress/StreamProgressInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.Stream_Progress
+{
+    public class StreamProgressInfo
+    {
+        private Stream file; 
+
+        public StreamProgressInfo(File file)
+        {
+            this.file = file;
+        }
+
+        public int CalculateCurrentPercent()
+        {
+            return (this.file.BytesSent * 100) / this.file.Length;
+        }
+    }
+}
