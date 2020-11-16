ef8b111ae06e04333ddbf5dfb92ea65328dddbd8
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2020-11-16 15:58:44 +0200

    Add exercise

---
+++ SOLID/Lab/03.DetailPrinter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03.DetailPrinter
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Employee> list = new List<Employee>();
+
+            Employee employee = new Manager("Ivan", new List<string> { "1", "2", "3" });
+            Employee employee2 = new Employee("Dragan");
+
+            list.Add(employee);
+            list.Add(employee2);
+
+            DetailsPrinter printer = new DetailsPrinter(list);
+
+            printer.PrintDetails();
+        }
+    }
+}
