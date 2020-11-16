98f26453267b587afec28535174f557273f1d2da
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2020-11-16 14:20:28 +0200

    Add exercise

---
+++ SOLID/Lab/02.GraphicEditor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace P02.Graphic_Editor
+{
+    class Program
+    {
+        static void Main()
+        {
+            GraphicEditor editor = new GraphicEditor();
+
+            Console.WriteLine(editor.DrawShape(new Rectangle()));
+        }
+    }
+}
