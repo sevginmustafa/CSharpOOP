d66326b9a03c30924cda70fd4e61371701257727
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2020-11-16 16:26:23 +0200

    Add exercise

---
+++ SOLID/Lab/04.Recharge/Robot.cs
@@ -0,0 +1,30 @@
+﻿namespace P04.Recharge
+{
+    public class Robot : Worker, IRechargeable
+    {
+        public Robot(string id, int capacity) : base(id)
+        {
+            this.Capacity = capacity;
+        }
+
+        public int Capacity { get; }
+
+        public int CurrentPower { get; set; }
+
+        public override void Work(int hours)
+        {
+            if (hours > this.CurrentPower)
+            {
+                hours = CurrentPower;
+            }
+
+            base.Work(hours);
+            this.CurrentPower -= hours;
+        }
+
+        public void Recharge()
+        {
+            this.CurrentPower = this.CurrentPower;
+        }
+    }
+}
