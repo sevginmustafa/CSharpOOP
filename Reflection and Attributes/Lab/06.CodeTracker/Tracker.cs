a78ec579e9fbe153544eb3fb0f249e3b9d196f2f
Sevgin Mustafa <sevgin_6_1996@abv.bg>
2020-11-19 20:03:25 +0200

    Add exercise

---
+++ Reflection and Attributes/Lab/06.CodeTracker/Tracker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public static void PrintMethodsByAuthor()
+        {
+            var type = typeof(StartUp);
+            var methods = type.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var method in methods)
+            {
+                if (method.CustomAttributes.Any(x => x.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+
+                    foreach (AuthorAttribute attribute in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is writen by {attribute.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
