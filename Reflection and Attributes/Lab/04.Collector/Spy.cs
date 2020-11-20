2be31da4fb4eaa56ce4a5d6d46b7d41e1910ce0e
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2020-11-20 08:39:39 +0200

    Add exercise

---
+++ Reflection and Attributes/Lab/04.Collector/Spy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string CollectGettersAndSetters(string investigatedClass)
+        {
+            Type classType = Type.GetType(investigatedClass);
+
+            MethodInfo[] classNonPublicMethods = classType.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var method in classNonPublicMethods.Where(x => x.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+
+            foreach (var method in classNonPublicMethods.Where(x => x.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
