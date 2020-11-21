6396d7889ba94bbf745cd65c26b15c39fe6b4864
Sevgin Mustafa <sevgin_6_1996@abv.bg>
2020-11-21 20:36:13 +0200

    Add exercise

---
+++ Reflection and Attributes/Exercise/02.ValidationAttributes/MyRangeAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        int minValue;
+        int maxValue;
+
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+        public override bool IsValid(object obj)
+        {
+            if (!(obj is int))
+            {
+                throw new ArgumentException();
+            }
+
+            int valueAsInt = (int)obj;
+
+            return valueAsInt >= 12 && valueAsInt <= 90;
+        }
+    }
+}
---
+++ Reflection and Attributes/Exercise/02.ValidationAttributes/Validator.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace ValidationAttributes
+{
+   public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            var properties = obj.GetType().GetProperties();
+
+            foreach (var property in properties)
+            {
+                var propertyCustomAttributes = property.GetCustomAttributes<MyValidationAttribute>();
+
+                foreach (var attribute in propertyCustomAttributes)
+                {
+                    bool result = attribute.IsValid(property.GetValue(obj));
+
+                    if (!result)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
