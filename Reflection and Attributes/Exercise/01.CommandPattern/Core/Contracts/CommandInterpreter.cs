03a902ffe6741a43cef31918974d9d05566619fc
Sevgin Mustafa <sevgin_6_1996@abv.bg>
2020-11-21 19:29:50 +0200

    Ädd exercise

---
+++ Reflection and Attributes/Exercise/01.CommandPattern/Core/Contracts/CommandInterpreter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Core.Contracts
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] splitted = args.Split();
+
+            string commandType = splitted[0];
+            string[] commandArguments = splitted.Skip(1).ToArray();
+
+            Type type = Assembly.GetCallingAssembly().GetTypes().FirstOrDefault(x => x.Name == commandType + "Command");
+
+            ICommand command = (ICommand)Activator.CreateInstance(type);
+
+            return command.Execute(commandArguments);
+        }
+    }
+}
---
+++ Reflection and Attributes/Exercise/01.CommandPattern/Core/Contracts/Engine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CommandPattern.Core.Contracts
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                string result = commandInterpreter.Read(input);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
