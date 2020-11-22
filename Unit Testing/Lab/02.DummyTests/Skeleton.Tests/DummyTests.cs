8bc6d204b3a41b7e9788328dd124705bacdad85e
Sevgin Mustafa <sevgin_6_1996@abv.bg>
2020-11-22 23:41:54 +0200

    Add exercise

---
+++ Unit Testing/Lab/02.DummyTests/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class DummyTests
+{
+    [Test]
+    public void DummyShouldLoseHealthIfAttacked()
+    {
+        //Arrange
+        Dummy dummy = new Dummy(100, 100);
+
+        //Act
+        dummy.TakeAttack(10);
+
+        //Assert
+        int expectedResult = 90;
+        int actualResult = dummy.Health;
+
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+
+    [Test]
+    public void DeadDummyShouldThrowInvalidOperationException()
+    {
+        //Arrange
+        Dummy dummy = new Dummy(0, 100);
+
+        //Act - Assert
+        Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(100));
+    }
+
+    [Test]
+    public void DeadDummyShouldGiveXP()
+    {
+        //Arrange
+        Dummy dummy = new Dummy(0, 100);
+
+        //Act - Assert
+        int expectedResult = 100;
+        int actualResult = dummy.GiveExperience();
+
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+
+    [Test]
+    public void AliveDummyShouldNotGiveXP()
+    {
+        //Arrange
+        Dummy dummy = new Dummy(100, 100);
+
+        //Act - Assert
+        Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+    }
+}
