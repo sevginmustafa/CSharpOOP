fd63957cf4ae15f9800314275154da279bf4b111
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2020-11-30 15:29:36 +0200

    Add exercise

---
+++ Design Patterns/Exercise/01.Prototype/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SandwichMenu sandwichMenu = new SandwichMenu();
+
+            sandwichMenu["BLT"] = new Sandwich("Wheat", "Bacon", "", "Lettuce, Tomato");
+            sandwichMenu["PR&J"] = new Sandwich("Whitet", "", "", "Peanut Butter, Jelly");
+            sandwichMenu["Turkey"] = new Sandwich("Rye", "Turkey", "Swiss", "Lettuce, Onion, Tomato");
+
+            sandwichMenu["LoadedBLT"] = new Sandwich("Wheat", "Turkey, Bacon", "American", "Lettuce, Tomato, Onion, Olives");
+            sandwichMenu["ThreeMeatCombo"] = new Sandwich("Rye", "Turkey, Ham, Salami", "Provolone", "Lettuce, Onion");
+            sandwichMenu["Vegetarian"] = new Sandwich("Wheat", "", "", "Lettuce, Onion, Tomato, Olives, Spinach");
+
+            Sandwich sandwich1 = sandwichMenu["BLT"].Clone() as Sandwich;
+            Sandwich sandwich2 = sandwichMenu["ThreeMeatCombo"].Clone() as Sandwich;
+            Sandwich sandwich3 = sandwichMenu["Vegetarian"].Clone() as Sandwich;
+        }
+    }
+}
