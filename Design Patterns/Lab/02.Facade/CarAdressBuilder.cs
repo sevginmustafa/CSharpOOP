a86d16193ff3e65e97380a0731579b1cd742029f
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2020-11-30 11:50:36 +0200

    Add exercise

---
+++ Design Patterns/Lab/02.Facade/CarAdressBuilder.cs
@@ -0,0 +1,22 @@
+﻿namespace _02.Facade
+{
+    public class CarAdressBuilder : CarBuilderFacade
+    {
+        public CarAdressBuilder(Car car)
+        {
+            Car = car;
+        }
+
+        public CarAdressBuilder InCity(string city)
+        {
+            Car.City = city;
+            return this;
+        }
+
+        public CarAdressBuilder AtAdress(string adress)
+        {
+            Car.Adress = adress;
+            return this;
+        }
+    }
+}
---
+++ Design Patterns/Lab/02.Facade/CarBuilderFacade.cs
@@ -0,0 +1,17 @@
+﻿namespace _02.Facade
+{
+    public class CarBuilderFacade
+    {
+        protected Car Car { get; set; }
+
+        public CarBuilderFacade()
+        {
+            Car = new Car();
+        }
+
+        public Car Build() => Car;
+
+        public CarInfoBuilder Info => new CarInfoBuilder(Car);
+        public CarAdressBuilder Built => new CarAdressBuilder(Car);
+    }
+}
