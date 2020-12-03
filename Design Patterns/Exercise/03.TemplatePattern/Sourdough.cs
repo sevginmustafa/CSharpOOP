1ebe1a228e69792bbfd7d7e0680506b376c43cfa
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2020-12-03 15:20:54 +0200

    Add exercise

---
+++ Design Patterns/Exercise/03.TemplatePattern/Sourdough.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03.TemplatePattern
+{
+    public class Sourdough : Bread
+    {
+        public override void Bake()
+        {
+            Console.WriteLine("Baking the Sourdough Bread. (20 minutes)");
+        }
+
+        public override void MixIngredients()
+        {
+            Console.WriteLine("Gathering Ingredients for Sourdough Bread.");
+        }
+    }
+}
---
+++ Design Patterns/Exercise/03.TemplatePattern/WholeWheat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03.TemplatePattern
+{
+    public class WholeWheat : Bread
+    {
+        public override void Bake()
+        {
+            Console.WriteLine("Baking the Whole Wheat Bread. (15 minutes)");
+        }
+
+        public override void MixIngredients()
+        {
+            Console.WriteLine("Gathering Ingredients for Whole Wheat Bread.");
+        }
+    }
+}
